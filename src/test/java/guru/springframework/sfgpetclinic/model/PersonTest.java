0c8686c3021c308979882da06b74b6afeffb8fea
savila <saul.avila@nyrhcloud.com>
2021-03-18 14:44:42 -0600

    Se agrega test

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,10 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 
 /**
  * Estos tags sirven para "agrupar" las pruebas y ejecutarlas conforme este
@@ -30,9 +35,9 @@ class PersonTest implements ModelTests {
 		// given
 		Person person = new Person(1l, "Joe", "Buck");
 
-		// then
-		assertAll("Test Props Set", () -> assertEquals("Joe", person.getFirstName(), "First Name Failed"),
-				() -> assertEquals("Buck", person.getLastName(), "Last Name Failed"));
+//then
+		assertAll("Test Props Set", () -> assertEquals(person.getFirstName(), "Joe", "First Name Failed"),
+				() -> assertEquals(person.getLastName(), "Buck", "Last Name Failed"));
 	}
 
 	/**
