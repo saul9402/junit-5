f835d2e3916d15b48e4de23da3d649cdef1c0ff1
savila <saul.avila@nyrhcloud.com>
2021-03-18 17:26:31 -0600

    Se agrega comentario sobre sufijo IT en nombre de clase

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -3,29 +3,36 @@ package guru.springframework.sfgpetclinic.services.springdatajpa;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Maven reconoce la clase como clase de test de integracion por el sufijo "IT"
+ * que lleva el nombre de la clase
+ * 
+ * @author savila
+ *
+ */
 class PetTypeSDJpaServiceIT {
 
-    @BeforeEach
-    void setUp() {
-    }
+	@BeforeEach
+	void setUp() {
+	}
 
-    @Test
-    void findAll() {
-    }
+	@Test
+	void findAll() {
+	}
 
-    @Test
-    void findById() {
-    }
+	@Test
+	void findById() {
+	}
 
-    @Test
-    void save() {
-    }
+	@Test
+	void save() {
+	}
 
-    @Test
-    void delete() {
-    }
+	@Test
+	void delete() {
+	}
 
-    @Test
-    void deleteById() {
-    }
+	@Test
+	void deleteById() {
+	}
 }
